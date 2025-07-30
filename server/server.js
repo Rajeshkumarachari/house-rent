3867a96e8236cf592367750bac1743a066669b70
Rajeshkumarachari <elakatarajesh83@gmail.com>
2025-07-30 20:59:35 +0530

    create sign up api route

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,8 +17,13 @@ mongoose
     console.log(err);
   });
 
+app.get("/", (req, res) => {
+  res.send("API is working...");
+});
+
 app.listen(3000, () => {
   console.log("API Working...");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
