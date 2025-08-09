5111e8770ecb2f54a7dd05c94f0345026bb9bed0
Rajeshkumarachari <elakatarajesh83@gmail.com>
2025-08-09 10:21:14 +0530

    complete upload listing images functionality

--- client/src/pages/BecomeAHost.jsx
+++ client/src/pages/BecomeAHost.jsx
@@ -1,10 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+// import { set } from "mongoose";
+import { app } from "../firebase";
+import { IoCloseOutline } from "react-icons/io5";
 
 const BecomeAHost = () => {
   const { currentUser } = useSelector((store) => store.user || {});
+  const [files, setFiles] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  //   console.log(files);
 
-  console.log(currentUser);
+  console.log(formData);
+  const handleUploadImage = (e) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 4) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError("Image upload failed :", err);
+          setUploading(false);
+        });
+    } else if (
+      files.length == 0 &&
+      files.length + formData.imageUrls.length == 0
+    ) {
+      setImageUploadError("Please select minimum 1 image ");
+    } else {
+      setImageUploadError("You can upload 3 images per listing");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          //   console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
   return (
     <main className=" p-3 max-w-4xl mx-auto h-full">
       <div className="">
@@ -143,13 +221,41 @@ const BecomeAHost = () => {
           <div className="flex mx-6">
             <fieldset className="fieldset">
               <span>Upload up to 3 images (Max size 2 MB each)</span>
-              <div className="flex   gap-7">
-                <input type="file" className="file-input" />
-                <button className=" bg-black text-white text-[16px] font-medium cursor-pointer  px-4 rounded-lg">
-                  Upload
+              <div className="flex gap-7">
+                <input
+                  onChange={(e) => setFiles(e.target.files)}
+                  type="file"
+                  className="file-input"
+                />
+                <button
+                  type="button"
+                  onClick={handleUploadImage}
+                  disabled={uploading}
+                  className=" bg-black text-white text-[16px] font-medium cursor-pointer  px-4 rounded-lg"
+                >
+                  {uploading ? "Uploading..." : "Upload"}
                 </button>
               </div>
             </fieldset>
+          </div>
+          <p className=" text-sm text-red-700 mx-6">
+            {imageUploadError && imageUploadError}
+          </p>
+          <div className="flex gap-5 mx-6 mt-3 ">
+            {formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((url, i) => (
+                <div className="relative inline-block group" key={i}>
+                  <img
+                    src={url}
+                    alt="photo"
+                    className=" size-28 object-cover cursor-pointer  rounded-lg"
+                  />
+                  <IoCloseOutline
+                    onClick={() => handleRemoveImage(i)}
+                    className=" z-20 bg-white absolute opacity-0 group-hover:opacity-100  -top-2 -right-2  hover:text-white hover:bg-red-500 size-5 cursor-pointer transition-opacity"
+                  />
+                </div>
+              ))}
           </div>
 
           <button className="bg-black text-white py-2 rounded-lg mx-7 mt-5 cursor-pointer hover:opacity-95 disabled:opacity-85">
